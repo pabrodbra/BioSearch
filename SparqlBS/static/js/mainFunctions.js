0027d9df66a2152381a6210b0d5b55149a13acd2
pabrodbra <pabrod@uma.es>
2017-12-17 02:32:05 +0100

    Prototype

--- SparqlBS/static/js/mainFunctions.js
+++ SparqlBS/static/js/mainFunctions.js
@@ -10,11 +10,11 @@ var test;
 
 // -- HELPERS
 var list_item_class = "list-group-item"
-var list_item_button_class = "btn btn-md btn-block btn-default"
+var list_item_button_class = "btn btn-md btn-default"
 
 var pathway_uri = "";
 var reaction_uri = "";
-var protein_uri = "";
+var controller_uri = "";
 
 
 function parseResponse(response){
@@ -25,22 +25,19 @@ function parseResponse(response){
 
 // -- SEARCHING
 
-function searchPathways(){
-	var input_pathway = document.getElementById("pathway_input").value;
-	var parsed_pathway = {}
-	console.log(input_pathway);
+function searchSPARQL(url, result_list, result_item, input_id){
+	var parsed_results = {}
+	console.log(input_id);
 
 	$.ajax({
-		url: 'pathway',
+		url: url,
 		type: "GET",
-		data: {'input':input_pathway},
+		data: {'input': input_id},
 		success:function(response){
-			parsed_pathway = parseResponse(response);
-			//var test = JSON.parse(response);
+			parsed_results = parseResponse(response);
 		},
 		complete:function(){
-			//appendPathways(parsed_pathway);
-			appendResults(parsed_pathway, "pathway_list")
+			appendResults(parsed_results, result_list, result_item)
 		},
 		error:function(e){
 			console.log("***ERROR*** :: " + e)
@@ -51,63 +48,117 @@ function searchPathways(){
 
 // -- CREATING
 
-function appendResults(parsed_response, box_id){
+function appendResults(parsed_response, box_id, child_class){
 	// Get List to Append
-	var pathway_list = document.getElementById(box_id);
-	while (pathway_list.firstChild) {
-		pathway_list.removeChild(pathway_list.firstChild);
-	}
+	var results_list = clearList(box_id);
+
 	variables = parsed_response.vars;
 	results = parsed_response.results;
-	console.log("DEBUG 1");
-	console.log(variables);
-	console.log(results);
-	test = variables;
 
 	for (var result in results){
 		var current_result = results[result]
 		// Create Elements
 		var new_li = document.createElement('li');
-		new_li.
 		new_li.className = list_item_class;
 		var new_button = document.createElement('button');
-		new_button.className = list_item_button_class;
-		console.log("DEBUG 2");
-		console.log(current_result)
+		new_button.className = list_item_button_class + " " + child_class;
 
 		var new_string = "";
 
 		//new_button.setAttribute("id", uri)
-		console.log("DEBUG 3");
 		// Create text from variables of results
 		for (var var_name in variables){
-			current_value = current_result[variables[var_name]]['value']
+			current_value = current_result[variables[var_name]]['value'];
 			if (var_name == 0){
-				new_button.setAttribute("id", current_result[variables[var_name]]['value'])
+				new_button.setAttribute("id", current_result[variables[var_name]]['value']);
 			}
-			else if (var_name == variables.length-1){
-				new_string += current_value
-			} else {
-				new_string += current_value + " | " ;
+			else{
+				current_value = current_value.split('#').pop();
+				if (var_name == variables.length-1){
+					new_string += current_value;
+				} else {
+					new_string += current_value + " | " ;
+				}
 			}
 		}
-		console.log(new_string);
 
 		// Add text to element
 		var new_text = document.createTextNode(new_string);
 		new_button.appendChild(new_text);
-		pathway_list.appendChild(new_button);
+		results_list.appendChild(new_button);
 	}
 }
 
 // -- ONCLICK
+var last_path = null, last_reaction = null, last_controller = null;
 
-$("#pathway_submit").click(function() {
-	searchPathways();
-});
+function activateButton(last_button, current_button){
+	if(last_button != null){
+		last_button.removeClass("btn-primary");
+		last_button.addClass("btn-default");
+	}
+	current_button.removeClass("btn-default");
+	current_button.addClass("btn-primary");
+}
+
+function clearList(list_id){
+	var selected_list = document.getElementById(list_id);
+	while (selected_list.firstChild) {
+		selected_list.removeChild(selected_list.firstChild);
+	}
+	return selected_list;
+}
+// - Pathways
 
 $("#pathway_input").keypress(function(e){
     if(e.which == 13){//Enter key pressed
         $('#pathway_submit').click();//Trigger search button click event
     }
+});
+
+$("#pathway_submit").click(function() {
+	clearList("reaction_list");
+	clearList("controller_list");
+	clearList("controller_info");
+
+	var input_pathway = document.getElementById("pathway_input").value;
+	searchSPARQL("pathway", "pathway_list", "pathway_item", input_pathway);
+});
+
+// - Reactions
+
+$(document).on('click', ".pathway_item", function() {
+	clearList("controller_list");
+	clearList("controller_info");
+
+	activateButton(last_path, $(this));
+	last_path = $(this);
+
+	pathway_uri = $(this).attr('id');
+	reaction_uri = null;
+	controller_uri = null;
+	searchSPARQL("reaction", "reaction_list", "reaction_item", pathway_uri);
+});
+
+// - Controllers
+
+$(document).on('click', ".reaction_item", function() {
+	clearList("controller_info");
+
+	activateButton(last_reaction, $(this));
+	last_reaction = $(this);
+
+	reaction_uri = $(this).attr('id');
+	controller_uri = null;
+	searchSPARQL("controller", "controller_list", "controller_item", reaction_uri);
+});
+
+// - Controller Info
+
+$(document).on('click', ".controller_item", function() {
+	activateButton(last_controller, $(this));
+	last_controller = $(this);
+
+	controller_uri = $(this).attr('id')
+	searchSPARQL("controller_info", "controller_info", "info_item", controller_uri);
 });
--- SparqlBS/urls.py
+++ SparqlBS/urls.py
@@ -7,7 +7,7 @@ from SparqlBS import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^pathway$', views.search_pathways, name='pathway'),
-    url(r'^reaction$', views.index, name='reaction'),
-    url(r'^controllers$', views.index, name='controllers'),
-    url(r'^protein$', views.index, name='protein'),
+    url(r'^reaction$', views.search_reactions, name='reaction'),
+    url(r'^controller$', views.search_controllers, name='controllers'),
+    url(r'^controller_info$', views.search_info_controllers, name='controller_info'),
 ]
